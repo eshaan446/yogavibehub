035a63804b899ab9c5fd7f744630626efb666523
Eshaan <eshaanpandeyk540@gmail.com>
2023-12-18 10:50:03 +0530

    default get added

--- server/index.js
+++ server/index.js
@@ -12,7 +12,59 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 app.use('/api',userRoute)
+
+
+
+//default get request
+app.get('/', (req, res) => {
+    const htmlContent = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <title>Your Web Page Title</title>
+        <!-- Add your CSS files, meta tags, or other headers here -->
+        <style>
+          /* Add your CSS styles here */
+          body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+            color: #333;
+          }
+  
+          /* Additional styles can be added as needed */
+          .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+          }
+  
+          h1 {
+            text-align: center;
+          }
+  
+          p {
+            line-height: 1.6;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <h1>Welcome to Your Website</h1>
+          <p>This is a simple HTML page served by Express.</p>
+          <!-- Add your content here -->
+        </div>
+        <!-- Add your scripts or other footer content here -->
+      </body>
+      </html>
+    `;
+    
+    res.send(htmlContent); // Send the HTML content as the response
+  });
